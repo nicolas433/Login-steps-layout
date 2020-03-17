e6b026fc6184a4b0e38ce9faef62289b95fceb93
Nicolas Grisoste <48197098+nicolas433@users.noreply.github.com>
2020-03-17 01:09:46 -0300

    Add files via upload

--- scripts/js.js
+++ scripts/js.js
@@ -1,60 +1,53 @@
 let progressBar = document.querySelectorAll("#progressbar > li");
 let forms = document.querySelectorAll("fieldset");
 
-let formControl = 0;
+let nextBtn = document.querySelectorAll(".action-button");
 
-let dataForm1 = document.querySelectorAll(
-  "form fieldset:nth-of-type(1) input:not([type='button'])"
-);
-let dataForm2 = document.querySelectorAll(
-  "form fieldset:nth-of-type(2) input:not([type='button']"
-);
-let dataForm3 = document.querySelectorAll(
-  "form fieldset:nth-of-type(3) input:not([type='button']"
-);
-let data = [dataForm1, dataForm2, dataForm3];
-
-console.log(data);
-
-function validate(elements) {
-  dataForm1 = document.querySelectorAll(
-    "form fieldset:nth-of-type(1) input:not([type='button'])"
+function validateForm(page) {
+  let validate = true;
+  let data = document.querySelectorAll(
+    `form fieldset:nth-of-type(${page + 1}) input:not([type='button'])`
   );
-  dataForm2 = document.querySelectorAll(
-    "form fieldset:nth-of-type(2) input:not([type='button']"
-  );
-  dataForm3 = document.querySelectorAll(
-    "form fieldset:nth-of-type(3) input:not([type='button']"
-  );
+  console.log(data);
 
-  console.log(elements);
-  for (let i = 0; i < elements.length; i++) {
-      console.log(elements[i].value)
-    if (elements[i].value == "") {
-      return false;
+  for (let i = 0; i < data.length; i++) {
+    if (String(data[i].value) == "") {
+      alert("Todos os campos são obrigatorios.");
+      validate = false;
+      return validate;
     }
   }
-}
+  
+  if (page == 0) {
+    if (String(data[0].value).length < 8) {
+      alert("Senha muito pequena.");
+      validate = false;
+      return validate;
+    }
 
-function npForm(a) {
-  if (!validate(data[formControl])) {
-    forms[formControl].style.display = "none";
-    progressBar[formControl].classList.remove("active");
-    formControl = formControl + a;
-    forms[formControl].style.display = "block";
-    progressBar[formControl].classList.add("active");
-  } else {
-    alert("Preencha todos os campos.");
+    if (String(data[1].value) != String(data[2].value)) {
+      alert("Senhas diferentes.");
+      validate = false;
+      return validate;
+    }
+    if (String(data[1].value).length < 8) {
+      alert("Senha muito pequena.");
+      return validate;
+    }
   }
+  return validate;
 }
 
-let nextBtn = document.querySelectorAll(".action-button");
-let previusBtn = document.querySelectorAll(".previus");
+function trocarDePagina(i, e) {
+  if (validateForm(i)) {
+    forms[i].style.display = "none";
+    progressBar[i].classList.remove("active");
+    forms[i + 1].style.display = "block";
+    progressBar[i + 1].classList.add("active");
+  }
+  e.preventDefault();
+}
 
 for (let i = 0; i < nextBtn.length; i++) {
-  nextBtn[i].addEventListener("click", () => npForm(1));
-}
-
-for (let i = 0; i < previusBtn.length; i++) {
-  previusBtn[i].addEventListener("click", () => npForm(-1));
+  nextBtn[i].addEventListener("click", () => trocarDePagina(i));
 }
